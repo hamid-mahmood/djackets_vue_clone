b1130aeeed08cd629dfd4a570fcff0d805501cc6
Hamid Mahmood Sial <hamidmahmoodsial@pk.folio3.com>
2023-08-15 06:42:42 -0700

    Cart, search and sign up functionality

--- src/App.vue
+++ src/App.vue
@@ -75,6 +75,14 @@
         </div>
       </div>
     </nav>
+
+    <div
+      class="is-loading-bar has-text-centered"
+      v-bind:class="{ 'is-loading': $store.state.isLoading }"
+    >
+      <div class="lds-dual-ring"></div>
+    </div>
+
     <section class="section">
       <router-view />
     </section>
@@ -89,11 +97,68 @@ export default {
   data() {
     return {
       showMobileMenu: false,
+      cart: {
+        items: [],
+      },
     };
+  },
+  beforeCreate() {
+    // commit is used to call mutations
+    this.$store.commit("initializeStore");
+  },
+  mounted() {
+    this.cart = this.$store.state.cart;
+  },
+  // it contains variables which are computed based on things around the component
+  computed: {
+    cartTotalLength() {
+      let totalLength = 0;
+      for (let i = 0; i < this.cart.items.length; i++) {
+        totalLength += this.cart.items[i].quantity;
+      }
+
+      return totalLength;
+    },
   },
 };
 </script>
 
 <style lang="scss">
 @import "../node_modules/bulma";
+.lds-dual-ring {
+  display: inline-block;
+  width: 80px;
+  height: 80px;
+}
+.lds-dual-ring:after {
+  content: " ";
+  display: block;
+  width: 64px;
+  height: 64px;
+  margin: 8px;
+  border-radius: 50%;
+  border: 6px solid #ccc;
+  border-color: #ccc transparent #ccc transparent;
+  animation: lds-dual-ring 1.2s linear infinite;
+}
+@keyframes lds-dual-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+.is-loading-bar {
+  height: 0;
+  overflow: hidden;
+
+  -webkit-transition: all 0.3s;
+  transition: all 0.3s;
+
+  &.is-loading {
+    height: 80px;
+  }
+}
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,45 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    cart: {
+      items: [],
+    },
+    isAuthenticated: false,
+    token: "",
+    isLoading: false,
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    // synchronous functions to change state
+    initializeStore(state) {
+      if (localStorage.getItem("cart")) {
+        state.cart = JSON.parse(localStorage.getItem("cart"));
+      } else {
+        localStorage.setItem("cart", JSON.stringify(state.cart));
+      }
+    },
+    addToCart(state, item) {
+      console.log("state", state);
+      console.log("item", item);
+      const exists = state.cart.items.filter(
+        (i) => i.product.id === item.product.id
+      );
+      if (exists.length) {
+        exists[0].quantity =
+          parseInt(exists[0].quantity) + parseInt(item.quantity);
+      } else {
+        state.cart.items.push(item);
+      }
+
+      localStorage.setItem("cart", JSON.stringify(state.cart));
+    },
+    setIsLoading(state, status) {
+      state.isLoading = status;
+    },
   },
   actions: {
+    // asynchronous functions to change state
   },
-  modules: {
-  }
-})
+  modules: {},
+});
